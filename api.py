36eb236f469f229477817c02009e954b78816c6d
ASergeevich <cs.muravey@gmail.com>
2025-05-12 09:16:20 +0300

    ttl is added explicitly and is displayed in the request

--- api.py
+++ api.py
@@ -15,8 +15,9 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 
 def fetch_crypto_data(cache_key="crypto_data", cache_ttl=60):
     cached_data = r.get(cache_key)
+    ttl_left = r.ttl(cache_key)
     if cached_data:
-        print("Cached data")
+        print(f"Cached data ({ttl_left} seconds left)")
         return json.loads(cached_data)
     
     print("Requesting info from CoinGecko")
